a61906671cc107cac9708e2b16580544c03f47ee
Psytch <contato.julio.agusso@gmail.com>
2025-04-02 00:47:10 -0300

    Update Ordenação exercises

--- src/collections/javaAPI/list/Ordenação/SomaNumeros.java
+++ src/collections/javaAPI/list/Ordenação/SomaNumeros.java
@@ -6,70 +6,79 @@ import java.util.List;
 public class SomaNumeros {
     //Atributos
 
-    @Override
-    public String toString() {
-        return "SomaNumeros: " +  " - Lista: " + numeroList;
-    }
-
     private List<Integer> numeroList;
 
     public SomaNumeros() {
         this.numeroList = new ArrayList<Integer>();
     }
 
+    // Adiciona um número à lista de números. (Funcionando)
     public void adicionarNumero(int numero) {
         this.numeroList.add(numero);
     }
 
+
+    // Retorna uma lista contendo todos os números presentes na lista. (Funcionando)
+    public void exibirNumeros() {
+        System.out.println("Números presentes na lista: " + numeroList);
+    }
+
+    // Calcula a soma de todos os números na lista e retorna o resultado. (Funcionando)
     public int calcularSoma() {
         int soma = 0;
         for (int i = 0; i < numeroList.size(); i++) {
             soma += numeroList.get(i);
         }
+        System.out.println("Soma: " + soma);
         return soma;
+
     }
 
-    public void encontrarMaiorNumero() {
-        int maior = 0;
+    //  Encontra o maior número na lista e retorna o valor. (Funcionando)
+    public int encontrarMaiorNumero() {
+        int maiorNumero = Integer.MIN_VALUE;
         for (int i = 0; i < numeroList.size(); i++) {
-            if (numeroList.get(i) > maior) {
-                maior = numeroList.get(i);
-            }
-            System.out.println("Maior numero: " + maior);
-        }
-    }
-
-    public void encontrarMenorNumero() {
-        int menor = 0;
-        for (int i = 0; i < numeroList.size(); i--) {
-            menor = numeroList.get(i);
-            if (menor < numeroList.get(i)) {
-                menor = numeroList.get(i);
+            if (numeroList.get(i) > maiorNumero) {
+                maiorNumero = numeroList.get(i);
             }
         }
-        System.out.println("Menor numero: " + menor);
+        System.out.println("Maior numero: " + maiorNumero);
+        return maiorNumero;
     }
 
-    public void exibirNumeros() {
-        System.out.println("Números presentes na lista: " + numeroList);
+    //  Encontra o menor número na lista e retorna o valor. (Funcionando)
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        for (int i = 0; i < numeroList.size(); i++) {
+            if (numeroList.get(i) < menorNumero) {
+                menorNumero = numeroList.get(i);
+            }
+        }
+        System.out.println("Maior numero: " + menorNumero);
+        return menorNumero;
     }
 
     public static void main(String[] args) {
         SomaNumeros somaNumeros = new SomaNumeros();
 
+        // Verificando se a lista está vazia
         somaNumeros.exibirNumeros();
+
+        // Adicionando Númeos à lista
         somaNumeros.adicionarNumero(1);
-        somaNumeros.adicionarNumero(55);
-        somaNumeros.adicionarNumero(40);
-        somaNumeros.adicionarNumero(400);
+        somaNumeros.adicionarNumero(9);
+        somaNumeros.adicionarNumero(10);
 
+        // Exibindo os números adicionados
+        somaNumeros.exibirNumeros();
+
+        // Realizando a soma dos númeoros da lista
+        somaNumeros.calcularSoma();
+
+        // Verificando o maior número
         somaNumeros.encontrarMaiorNumero();
-        //somaNumeros.encontrarMenorNumero();
 
-
-
+        // Verificando o menor número
+        somaNumeros.encontrarMenorNumero();
     }
-
-
-
 }
